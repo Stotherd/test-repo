6b7e578a59e372e6abe380ec333734dd8029710d
lelmore <lelmore@shopkeep.com>
2018-01-02 17:13:18 +0000

    Adding notification email code to newly located release_gli.rb

--- ci-scripts/git_tools/lib/cli/release_gli.rb
+++ ci-scripts/git_tools/lib/cli/release_gli.rb
@@ -26,9 +26,22 @@ module Gitkeep
         git_utilities = GitUtils.new(logger)
         release_cutter = CutRelease.new(logger, git_utilities, options)
         release_cutter.cut_release
-        # inform stakeholders * needs email libs and mailing list, and possible slack integration (new file(s) notification?
+        notification = Notification.new(logger, git_utilities, options)
+        notification.email_branch_creation
         # do Jira stuff (TBD)
       end
+    end
+    command :notify_branch_creation do |c|
+       c.desc 'The new release branch name'
+       c.flag %i[v version], type: String
+
+       c.action do |_global_option, options, _args|
+           logger = Logger.new(STDOUT)
+           logger.info "Sending branch creation notification: #{options[:version]}"
+           git_utilities = GitUtils.new(logger)
+           notification = Notification.new(logger, git_utilities, options)
+           notification.email_branch_creation
+       end
     end
   end
 end
